e10566252ed521fe44634966136aaac62ba99ced
Alex Beregszaszi <alex@rtfs.hu>
2019-09-28 00:04:03 +0100

    Show debug functionality in helloworld example

--- scripts/helloworld/src/lib.rs
+++ scripts/helloworld/src/lib.rs
@@ -7,6 +7,13 @@ use ewasm_api::*;
 pub extern "C" fn main() {
     let pre_state_root = eth2::load_pre_state_root();
 
+    // Show debug functionality
+    debug::log("hello world!");
+    debug::print32(42);
+    debug::print64(99);
+    debug::print_mem(&pre_state_root.bytes);
+    debug::print_mem_hex(&pre_state_root.bytes);
+
     assert!(eth2::block_data_size() == 0);
 
     // No updates were made to the state
